f95de5135e0b10e9d18d647ba36577908efed522
Alexey Kolomeytsev <zombiepro66@gmail.com>
2025-08-15 15:25:26 +0300

    return tasklist and task after reset hard

--- src/model/TaskList.cpp
+++ src/model/TaskList.cpp
@@ -4,8 +4,17 @@
 
 #include "TaskList.h"
 
+#include <iostream>
+
 TaskList::TaskList() {
-    // loadFromJson("TaskList.json");
+ if (jArray.empty()) {
+     try {
+         loadFromJson("tasks.json");
+     } catch (std::exception &e) {
+         std::cout << e.what() << '\n';
+     }
+
+ }
 }
 
 void TaskList::AddTask(const std::string &newTitle, const std::string &newDescription, const wxDateTime & newFinishDate, bool completed) {
@@ -14,4 +23,67 @@ void TaskList::AddTask(const std::string &newTitle, const std::string &newDescri
 }
 
 bool TaskList::removeTask(int id) {
+    auto key = tasks.find(id);
+    if (key != tasks.end()) {
+        tasks.erase(id);
+        return true;
+    }
+    return false;
+}
+bool TaskList::editTask(int id, const std::string &newTitle, const std::string newDescription, const wxDateTime &newFinisDate, bool completed) {
+    auto key = tasks.find(id);
+    if (key != tasks.end()) {
+        key->second.setTitle(newTitle);
+        key->second.setDescription(newDescription);
+        key->second.setDateFinish(newFinisDate);
+        key->second.setComplete(completed);
+        return true;
+    }
+    return false;
+}
+
+Task *TaskList::getTask(int id)  {
+    auto key = tasks.find(id);
+    if (key != tasks.end()) {
+        return &(key->second);
+    }
+    return nullptr;
+}
+
+bool TaskList::loadFromJson(const std::string& path) {
+    std::fstream file(path);
+    if (!file.is_open() || !file.good()) {
+        return false;
+    }
+    file >> jArray;
+    for (const auto& item : jArray) {
+        std::string title = item["title"];
+        std::string description = item["description"];
+        bool completed = item["completed"];
+        wxDateTime startDate;
+        wxDateTime finishDate;
+        startDate.ParseISOCombined(item["startDate"].get<std::string>());
+        finishDate.ParseISOCombined(item["finishDate"].get<std::string>());
+        Task t(title, description, finishDate, completed);
+        tasks.emplace(t.getId(), t);
+    }
+    return true;
+}
+
+bool TaskList::saveToJson(const std::string& path) {
+    std::fstream file(path);
+    if (!file.is_open() || !file.good()) {
+        return false;
+    }
+    nlohmann::json newJArray = nlohmann::json::array();
+    for (int i = 0; i < tasks.size(); i++) {
+        newJArray.push_back({
+        {"title", tasks[i].getTitle()},
+        {"description", tasks[i].getDescription()},
+        {"completed", tasks[i].getComplete},
+        {"finishDate", tasks[i].getDateFinish().FormatISOCombined(' ')},
+        });
+
+    }
+    return true;
 }
