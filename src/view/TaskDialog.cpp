d83a7820e4f36ce6b87846bd838bf7e72007099a
Alexey Kolomeytsev <zombiepro66@gmail.com>
2025-08-18 22:07:49 +0300

    **Made some function for TaskDialog after clicking button "Создать задачу", opened new dialog with option
    *still in development

--- src/view/TaskDialog.cpp
+++ src/view/TaskDialog.cpp
@@ -3,3 +3,59 @@
 //
 
 #include "TaskDialog.h"
+
+
+void TaskDialog::InitUI(Task *task) {
+    if (task != nullptr) {
+        titleCtrl->SetValue(task->getTitle());
+        descCtrl->SetValue(task->getDescription());
+        dateCtrl->SetValue(task->getDateFinish());
+        completeCheck->SetValue(task->getCompleted());
+    } else {
+        titleCtrl = new wxTextCtrl(this, wxID_ANY);
+        descCtrl = new wxTextCtrl(this, wxID_ANY, "", wxDefaultPosition, wxDefaultSize, wxTE_MULTILINE);
+        dateCtrl = new wxDatePickerCtrl(this, wxID_ANY);
+        completeCheck = new wxCheckBox(this, wxID_ANY, "Статус");
+    }
+    // 1. Сетка для меток и полей
+    auto* grid = new wxFlexGridSizer(2, 10, 10); // 2 колонки
+    grid->Add(new wxStaticText(this, wxID_ANY, "Название:"));
+    grid->Add(titleCtrl);
+    grid->Add(new wxStaticText(this, wxID_ANY, "Описание"));
+    grid->Add(descCtrl);
+    grid->Add(new wxStaticText(this, wxID_ANY, "Дата"));
+    grid->Add(dateCtrl);
+
+    // 2. Общий вертикальный sizer
+    auto* vbox = new wxBoxSizer(wxVERTICAL);
+    vbox->Add(grid, 0, wxALL | wxEXPAND, 10);
+    vbox->Add(completeCheck, 0, wxALL, 10);
+
+    auto* ok = new wxButton(this, wxID_OK, "ОК");
+    auto* cancel = new wxButton(this, wxID_CANCEL, "Отмена");
+
+    vbox->Add(ok, 0, wxALL, 10);
+    vbox->Add(cancel, 0, wxALL, 10);
+
+    SetSizerAndFit(vbox);
+    Layout();
+    Centre(); // по центру экрана
+}
+
+wxString TaskDialog::GetTitleValue() const {
+    return titleCtrl->GetValue();
+}
+
+wxString TaskDialog::GetDescriptionValue() const {
+    return descCtrl->GetValue();
+}
+
+wxDateTime TaskDialog::GetDateValue() const {
+    return dateCtrl->GetValue();
+}
+
+bool TaskDialog::GetCompletedValue() const {
+    return completeCheck->GetValue();
+}
+
+
--- src/view/TaskDialog.h
+++ src/view/TaskDialog.h
@@ -5,12 +5,42 @@
 #ifndef TASKDIALOG_H
 #define TASKDIALOG_H
 
+#include <wx/dialog.h>         // базовый класс wxDialog
+#include <wx/textctrl.h>       // для title и description
+#include <wx/datectrl.h>       // базовый класс для wxDatePickerCtrl
+#include <wx/dateevt.h>        // события от даты
+#include <wx/checkbox.h>       // для галочки "выполнено"
+#include <wx/sizer.h>          // компоновка (BoxSizer / GridSizer)
+#include <wx/button.h>         // кнопки OK / Cancel
+#include <wx/stattext.h>       // подписи к полям
+#include <wx/msgdlg.h>         // всплывающие окна (ошибки)
+#include <wx/datetime.h>       // работа с датой
+#include "../model/Task.h"     // если хочешь передавать/редактировать задачу
 
 
-class TaskDialog {
 
+class TaskDialog : public wxDialog {
+public:
+    TaskDialog(wxWindow* parent, const wxString& title, Task* task = nullptr);
+
+    wxString GetTitleValue() const;
+    wxString GetDescriptionValue() const;
+    wxDateTime GetDateValue() const;
+    bool GetCompletedValue() const;
+
+private:
+    wxTextCtrl* titleCtrl = nullptr;
+    wxTextCtrl* descCtrl = nullptr;
+    wxDatePickerCtrl* dateCtrl = nullptr;
+    wxCheckBox* completeCheck = nullptr;
+
+    void InitUI(Task* task = nullptr); // если task != nullptr → редактирование
+
+    void OnOk(wxCommandEvent& event);
+    void OnCancel(wxCommandEvent& event);
+
+    wxDECLARE_EVENT_TABLE();
 };
-
 
 
 #endif //TASKDIALOG_H
