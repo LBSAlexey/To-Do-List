4c598021f6a27a7072c205fdf803e79e2b5b5ef1
Alexey Kolomeytsev <zombiepro66@gmail.com>
2025-08-19 12:31:10 +0300

    Made function for TaskDialog
    *open new model window, after clicked button "Создать задачу". In there window you may add new task

--- src/view/TaskDialog.cpp
+++ src/view/TaskDialog.cpp
@@ -6,24 +6,20 @@
 
 
 void TaskDialog::InitUI(Task *task) {
+    titleCtrl = new wxTextCtrl(this, wxID_ANY);
+    descCtrl = new wxTextCtrl(this, wxID_ANY, "", wxDefaultPosition, wxDefaultSize, wxTE_MULTILINE);
+    dateCtrl = new wxDatePickerCtrl(this, wxID_ANY);
+    completeCheck = new wxCheckBox(this, wxID_ANY, "Статус");
     if (task != nullptr) {
         titleCtrl->SetValue(task->getTitle());
         descCtrl->SetValue(task->getDescription());
         dateCtrl->SetValue(task->getDateFinish());
         completeCheck->SetValue(task->getCompleted());
-    } else {
-        titleCtrl = new wxTextCtrl(this, wxID_ANY);
-        descCtrl = new wxTextCtrl(this, wxID_ANY, "", wxDefaultPosition, wxDefaultSize, wxTE_MULTILINE);
-        dateCtrl = new wxDatePickerCtrl(this, wxID_ANY);
-        completeCheck = new wxCheckBox(this, wxID_ANY, "Статус");
     }
     // 1. Сетка для меток и полей
     auto* grid = new wxFlexGridSizer(2, 10, 10); // 2 колонки
-    grid->Add(new wxStaticText(this, wxID_ANY, "Название:"));
     grid->Add(titleCtrl);
-    grid->Add(new wxStaticText(this, wxID_ANY, "Описание"));
     grid->Add(descCtrl);
-    grid->Add(new wxStaticText(this, wxID_ANY, "Дата"));
     grid->Add(dateCtrl);
 
     // 2. Общий вертикальный sizer
@@ -58,4 +54,21 @@ bool TaskDialog::GetCompletedValue() const {
     return completeCheck->GetValue();
 }
 
+void TaskDialog::OnOk(wxCommandEvent &event) {
+    wxString title = titleCtrl->GetValue();
+    wxDateTime date = dateCtrl->GetValue();
+    if (title.empty()) {
+        wxMessageBox("Название задачи не может быть пустым");
+        return;
+    }
+    if (date.IsEarlierThan(wxDateTime::Now()) || date.IsEqualTo(wxDateTime::Now())) {
+        wxMessageBox("Время не может выставлено ранее или в настоящее время");
+        return;
+    }
+    EndModal(wxID_OK);
+}
+
+void TaskDialog::OnCancel(wxCommandEvent &event) {
+    EndModal(wxID_CANCEL);
+}
 
